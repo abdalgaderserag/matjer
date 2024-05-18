4f42101ef32f0715a58e75020cc6811a3d8a7b84
Abd Al-Gader Sirag <abdalgadersir@gmail.com>
2024-05-18 21:29:47 +0200

    order validation

--- app/Http/Requests/StoreOrderRequest.php
+++ app/Http/Requests/StoreOrderRequest.php
@@ -3,6 +3,7 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\Auth;
 
 class StoreOrderRequest extends FormRequest
 {
@@ -11,7 +12,8 @@ class StoreOrderRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        $user = Auth::getUser();
+        return $user['type'] == 0;
     }
 
     /**
@@ -22,7 +24,7 @@ class StoreOrderRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'items' => 'required|Array'
         ];
     }
 }
--- app/Http/Requests/UpdateOrderRequest.php
+++ app/Http/Requests/UpdateOrderRequest.php
@@ -3,6 +3,7 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\Auth;
 
 class UpdateOrderRequest extends FormRequest
 {
@@ -11,7 +12,8 @@ class UpdateOrderRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        $user = Auth::getUser();
+        return $user['type'] == 0;
     }
 
     /**
@@ -22,7 +24,7 @@ class UpdateOrderRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'items' => 'required|Array'
         ];
     }
 }
