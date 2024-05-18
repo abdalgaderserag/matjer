dbcd9fb695095a4df98b8e51be8e473308f19b9d
Abd Al-Gader Sirag <abdalgadersir@gmail.com>
2024-05-18 19:05:00 +0200

    coding item model

--- app/Models/Item.php
+++ app/Models/Item.php
@@ -8,4 +8,51 @@ use Illuminate\Database\Eloquent\Model;
 class Item extends Model
 {
     use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'name',
+        'value',
+        'category',
+        'count',
+        'curr_type',
+        'details',
+        'images',
+    ];
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @return string
+     * @var $cost
+     *
+     * out put price with the right type of coin
+     *
+     */
+    public function getCountAttribute($cost)
+    {
+        return config('matjer.curr[' . $this->curr_type . ']',) . ' ' . $cost;
+//        return '$ ' . $cost;
+    }
+
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @return string
+     * @var $category
+     *
+     * out put price with the right type of coin
+     *
+     */
+    public function getCategoryAttribute($category)
+    {
+        return config('matjer.category[' . $category . ']', 'other');
+
+    }
+
 }
---
+++ config/matjer.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'curr' => ['$' , 'USD'],
+    'category' => [
+        'pre-built',
+        'gpus',
+        'cpus',
+        'RAMs and Storage',
+        'cooling',
+        'cases and PSU',
+        'Others'
+    ]
+];
