b2ca867107448c197fce57df41328ed96c5702ad
Abd Al-Gader Sirag <abdalgadersir@gmail.com>
2024-05-19 21:50:57 +0200

    some littel edits in item index

--- resources/views/components/item.blade.php
+++ resources/views/components/item.blade.php
@@ -1,13 +1,13 @@
 <div class="card">
     <img class="item-image" alt="{{ $item['name'] }}" src="{{ $item['images'] }}">
-    <div>{{ $item['name'] }}</div>
-    <div>
+    <div class="card-info">
+        <span class="card-title">{{ $item['name'] }}</span><br>
         <span class="green">{{ $item['value'] }}</span>
 
         @if($item['count'] == 1)
-            | only {{ $item['count'] }} left!
+            <span>|</span> only {{ $item['count'] }} left!
         @elseif($item['count'] == 0)
-            | <span class="red">out of stock</span>
+            <span>|</span> <span class="red">out of stock</span>
         @endif
     </div>
 </div>
