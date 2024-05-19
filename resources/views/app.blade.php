fe1dce8892ff9f608ef07adc1edd216979ba5b33
Abd Al-Gader Sirag <abdalgadersir@gmail.com>
2024-05-19 16:53:26 +0200

    add the basic app view code

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,3 +1,25 @@
-<div>
-    <!-- Simplicity is an acquired taste. - Katharine Gerould -->
+@in
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>@yield('title','mat\'jer')</title>
+    @section('style')
+    @show
+</head>
+<body>
+@include('component.header')
+<div id="main">
+    <div class="section"></div>
+    @yield('main')
+    @include('component.slide')
 </div>
+@include('component.footer')
+</body>
+@section('script')
+@show
+</html>
